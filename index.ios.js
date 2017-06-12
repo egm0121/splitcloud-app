83b5086792f2ac032bfefe62bee765b0fd876b55
Giulio Dellorbo <egm0121@gmail.com>
2017-06-12 15:03:45 +0100

    added Ga analytics tracking of screenView events

--- index.ios.js
+++ index.ios.js
@@ -8,16 +8,23 @@ import {
   Navigator,
   View
 } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 import { Provider } from 'react-redux';
 import MainSceneContainer from './containers/mainSceneContainer';
 import NotificationContainer from './containers/notificationContainer';
-
+import {
+  Analytics,
+  Hits as GAHits
+} from 'react-native-google-analytics';
 import { store } from './redux/store/configure';
 
 class SplitCloudApp extends Component {
   constructor(props){
     super(props);
     this.configureScene = this.configureScene.bind(this);
+    let clientId = DeviceInfo.getUniqueID();
+    this.googTracker = new Analytics('UA-100899493-2', clientId, 1, DeviceInfo.getUserAgent());
+    console.log('device info',DeviceInfo.getUserAgent(),'uniqueId',clientId );
   }
   configureScene(route, routeStack){
     return {
@@ -31,6 +38,13 @@ class SplitCloudApp extends Component {
             <Navigator
                 initialRoute={{ title: 'Initial screen', index: 0, component: MainSceneContainer }}
                 renderScene={(route, navigator) => {
+                  var screenView = new GAHits.ScreenView(
+                       'SplitcloudApp',
+                       route.component.displayName,
+                       DeviceInfo.getReadableVersion(),
+                       DeviceInfo.getBundleId()
+                     );
+                  this.googTracker.send(screenView);
                   let Component = route.component;
                   return <View style={{flex: 1}}>
                           <Component title={route.title} navigator={navigator} {...route.passProps}/>
