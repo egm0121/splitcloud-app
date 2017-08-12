8409eae03b5d7c76fe3e8c91309caf0de9725947
Giulio Dellorbo <egm0121@gmail.com>
2017-08-12 16:09:05 +0100

    refactored buttons, new Button component WIP

--- components/backButton.js
+++ components/backButton.js
@@ -2,37 +2,11 @@
  * @flow
  */
 import React, { PropTypes, Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity
-} from 'react-native';
-import THEME from '../styles/variables';
+import { StyleSheet } from 'react-native';
+import Button from './button';
 
 function BackButton(props){
-  let buttonContainerStyle = props.style ?
-    [styles.closeButtonContainer,props.style]:
-    [styles.closeButtonContainer];
-  return <TouchableOpacity style={buttonContainerStyle} onPress={props.onPressed}>
-          <Image style={[styles.closeButtonIcon]} source={require('../assets/flat_back_btn.png')} resizeMode={'cover'}/>
-      </TouchableOpacity>;
+  return <Button image={require('../assets/flat_back_btn.png')} {...props} />
+}
 
-}
-BackButton.propTypes ={
-  'onPressed' : PropTypes.func.isRequired
-}
-const styles = StyleSheet.create({
-  closeButtonContainer :{
-    position:'absolute',
-    left:0,
-    paddingLeft:10,
-    top:20
-  },
-  closeButtonIcon:{
-    width:30,
-    height:30
-  }
-});
 export default BackButton;
