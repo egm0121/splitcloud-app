05a452e40546997d5722a96cac841d5ac6adc90f
Giulio Dellorbo <egm0121@gmail.com>
2018-11-27 08:44:21 -0500

    added adMob framework + updated react-native-audio-streaming

--- android/app/src/main/java/com/example/MainApplication.java
+++ android/app/src/main/java/com/example/MainApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import cl.json.RNSharePackage;
 import com.bugsnag.BugsnagReactNative;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new RNSharePackage(),
             BugsnagReactNative.getPackage(),
             new RNSoundPackage(),
