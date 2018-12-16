5ffd7fddb962946e7deb3b7d2810c2f93dc6d10d
Giulio Dellorbo <egm0121@gmail.com>
2018-12-16 10:11:56 -0500

    lighter bg blurred artwork overlay to compensate for dark artworks in fg

--- styles/variables.js
+++ styles/variables.js
@@ -9,7 +9,7 @@ const styles = {
   contentBgColor : 'rgb(18, 19, 20)',
   contentBorderColor : 'rgba(71, 71, 72, 0.3)',
   tabBarBorderColor : '#383838',
-  textOverlayBgColor : 'rgba(18, 19, 20,0.5)',
+  textOverlayBgColor : 'rgba(69, 73, 78, 0.5)',
   imageTextOverlayBgColor : 'rgba(41,41,46,0.7)',
   notifyBgColor : 'rgba(41,41,46,0.9)',
   listBorderColor : '#39393a',
