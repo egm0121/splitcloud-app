7263e48d6a6964ce8db76f02ac28532f519a7bf8
Giulio Dellorbo <egm0121@gmail.com>
2019-02-08 09:14:29 -0500

    add start stop methods to StreamTokenManager

--- modules/StreamTokenManager.js
+++ modules/StreamTokenManager.js
@@ -1,13 +1,25 @@
 import SplitCloudApi from './SplitcloudApi';
 import { updateActiveStreamToken } from './SoundcloudApi';
+import config from '../helpers/config';
 import throttle from 'lodash.throttle';
 
 class StreamTokenManager {
   constructor(){
     this.api = new SplitCloudApi();
     this.checkActiveToken = throttle(this.checkActiveToken.bind(this),5000,{leading:true});
+    this.enabled = true;
+  }
+  stop(){
+    this.enabled = false;
+  }
+  start(){
+    this.enabled = true;
   }
   checkActiveToken(){
+    if (!this.enabled){
+      console.log('StreamTokenManager is disabled');
+      return Promise.resolve(false); 
+    } 
     console.log('checkActiveToken called');
     this.api.getApplicationConfig().then( config => {
       if( typeof config === 'object' && 
@@ -19,5 +31,8 @@ class StreamTokenManager {
   }
 }
 const managerService = new StreamTokenManager;
+if (config.OVERRIDE_STREAM_TOKEN ){
+  managerService.stop();
+}
 managerService.checkActiveToken();
 export default managerService;
